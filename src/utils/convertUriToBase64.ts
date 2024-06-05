544f0aa09b38bf359bca68d5a4a5b4f53cd2dcf1
Vinicius Leite <viniciusleite@brq.com>
2024-06-05 13:59:13 -0300

    fix: correcao de upload de image web

--- src/utils/convertUriToBase64.ts
+++ src/utils/convertUriToBase64.ts
@@ -1,11 +1,28 @@
 import * as FileSystem from "expo-file-system"
+import { Platform } from "react-native"
 
 export async function convertUriToBase64(uri: string) {
 	try {
-		const base64 = await FileSystem.readAsStringAsync(uri, {
-			encoding: FileSystem.EncodingType.Base64,
+		const isWeb = Platform.OS === "web"
+		if (!isWeb) {
+			const base64 = await FileSystem.readAsStringAsync(uri, {
+				encoding: FileSystem.EncodingType.Base64,
+			})
+			return base64
+		}
+
+		const response = await fetch(uri)
+		const blob = await response.blob()
+		const reader = new FileReader()
+		const base64 = await new Promise((resolve, reject) => {
+			reader.onerror = reject
+			reader.onload = () => {
+				resolve(reader.result)
+			}
+			reader.readAsDataURL(blob)
 		})
-		return base64
+
+		return String(base64)
 	} catch (error) {
 		throw error
 	}
--- src/utils/getFileExtension.ts
+++ src/utils/getFileExtension.ts
@@ -1,4 +1,11 @@
+import { Platform } from "react-native"
+
 export function getExtension(uri: string): string {
+	const isWeb = Platform.OS === "web"
+	if (isWeb) {
+		const parts = uri.split(";")[0].split("/")
+		return "." + parts[1]
+	}
 	const url = new URL(uri)
 	const parts = url.pathname.split(".")
 	return "." + parts[parts.length - 1]
